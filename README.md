bc31ce717eff6ec0f67ea577d927fa39f4b852c2
ValleyDragon888 <valleydragon888@gmail.com>
2023-07-23 10:36:18 +0100

    Moved the patterns and added a tutorial to main.py

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RGB-OS
 A circuitpython project for easily creating Neopixel devices.
+Usage is simple: RGB-OS defines a series of patterns. These are classes, derived from `Animation()`.
+Upon calling the function `next_frame()` the class will write the next frame to the neopixels (passed into the class upon `__init__()`)
+The `main.py` file first makes a list of the animations, and starts playing the first one. If you press the button 
+(defined in `main.py`), it stops playing the first animation, and moves on to the next. Pressing it again will play the third.
+
+## Usage
+To use, first install circuitpython on a microcontroller, and attatch neopixels. Then, erase all of the files on the
+microcontroller, and replace with the files in this repo (even the `lib` folder). Then, open `main.py` in a text editor, 
+and follow the steps detailed in the file.
--- main.py
+++ main.py
@@ -1,30 +1,48 @@
+# Welcome to the main.py file. Here are the steps necessary to get RGB OS up and running
+# Just jump your eyes over the imports. (To JUMP TO HERE)
+# Also please read the README.md before doing this.
+
 import board
 import digitalio
 import time
 import neopixel
 
 # import animations
-from colourchase import ColourChase
-from SoftFade import SoftFade
-from GentleGlow import GentleGlow
-from SingleChase import SingleChase
+from patterns.colourchase import ColourChase
+from patterns.SoftFade import SoftFade
+from patterns.GentleGlow import GentleGlow
+from patterns.SingleChase import SingleChase
 
-# init neopixels and button
+# --------------------------JUMP TO HERE-------------------------
+# First, edit these variables to your needs:
+# This one is fairy self-explanitory. Input the number of neopixels you have.
 num_pixels = 50
+# Next, edit 'GP0' to the GPIO pin that the data pin of your neopixels are connected to.
 pixels = neopixel.NeoPixel(board.GP0, num_pixels)
+# If you find it's a problem, you cn change this: (0-1)
 pixels.brightness = 0.5
 
+# Now, the button is used to switch between different animations.
+# On the next line, change 'GP1' to the gpio pin your button is connected to,
 button = digitalio.DigitalInOut(board.GP1)
 button.direction = digitalio.Direction.INPUT
+# If your button is connected to power, don't change anything on the next bit of code.
+# However, if you have connected it to ground, change 'DOWN' to 'UP'
 button.pull = digitalio.Pull.DOWN
 
-# make an instance of each animaton
+# Here is the list of animations, that you flip between by pressing the button
+# If you would not like your device to use a certian animation, just remove the line.
+# (To work out which one to remove, work out how many times you have to press the button get to it from switchon.)
+
 animations = [
         ColourChase(pixels, num_pixels),
         SoftFade(pixels, num_pixels),
         GentleGlow(pixels, num_pixels),
         SingleChase(pixels, num_pixels)
     ]
+
+# -----------------------END OF EDITABLE SECTION--------------------------
+
 num = 0
 
 def button_is_pressed():
